31bfa4e241a1a99cece0a40132b1786d0f2d70ff
Uday P <uday.patial@thescore.com>
2022-03-22 22:56:12 -0400

    Add tests

---
+++ FrankZappaUd/FrankZappa.swift
@@ -0,0 +1,20 @@
+//
+//  FrankZappa.swift
+//  FrankZappaUd
+//
+//  Created by Uday Patial on 2022-03-22.
+//
+
+public final class FrankZappa {
+
+	let name = "FrankZappa"
+
+	public func add(a: Int, b: Int) -> Int {
+		return a + b
+	}
+
+	public func sub(a: Int, b: Int) -> Int {
+		return a - b
+	}
+
+}
--- FrankZappaUdTests/FrankZappaUdTests.swift
+++ FrankZappaUdTests/FrankZappaUdTests.swift
@@ -10,24 +10,18 @@ import XCTest
 
 class FrankZappaUdTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+	var frankZappa: FrankZappa!
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+	override func setUp() {
+		frankZappa = FrankZappa()
+	}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+	func testAdd() {
+		XCTAssertEqual(frankZappa.add(a: 1, b: 1), 2)
+	}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+	func testSub() {
+		XCTAssertEqual(swiftyLib.sub(a: 2, b: 1), 1)
+	}
 
 }
