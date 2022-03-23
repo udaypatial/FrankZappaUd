6601d801acce35ce755f24b85142e13610e3e320
Uday P <uday.patial@thescore.com>
2022-03-22 22:48:58 -0400

    Initial project setup

---
+++ FrankZappaUd/FrankZappaUd.h
@@ -0,0 +1,18 @@
+//
+//  FrankZappaUd.h
+//  FrankZappaUd
+//
+//  Created by Uday Patial on 2022-03-22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrankZappaUd.
+FOUNDATION_EXPORT double FrankZappaUdVersionNumber;
+
+//! Project version string for FrankZappaUd.
+FOUNDATION_EXPORT const unsigned char FrankZappaUdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrankZappaUd/PublicHeader.h>
+
+
---
+++ FrankZappaUdExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FrankZappaUdExamples
+//
+//  Created by Uday Patial on 2022-03-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
